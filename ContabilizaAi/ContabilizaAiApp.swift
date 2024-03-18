ff94b7fa88378cd4ca4649d10e5e1672b02a689b
Guilherme Martins <academicgmag@gmail.com>
2024-03-18 17:27:29 -0300

    Initial Commit

---
+++ ContabilizaAi/ContabilizaAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContabilizaAiApp.swift
+//  ContabilizaAi
+//
+//  Created by Guilherme Alves on 18/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ContabilizaAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContabilizaAi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContabilizaAi
+//
+//  Created by Guilherme Alves on 18/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
